6d9a5f991e2c4fe1ce9fad51aad2d4947088937a
unknown <flaviosenne123@gmail.com>
2022-08-17 10:19:31 -0300

    alterar docker compose, adicionar script do banco

---
+++ codebank/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE credit_cards (
+    id uuid NOT NULL,
+    name VARCHAR NOT NULL,
+    number VARCHAR NOT NULL,
+    expiration_month VARCHAR NOT NULL,
+    expiration-year VARCHAR,
+    balance FLOAT NOT NULL,
+    balance_limit FLOAT NOT NULL,
+    PRIMARY KEY (id) 
+);
+
+CREATE TABLE transactions (
+    id uuid NOT NULL,
+    credit_card_id uuid NOT NULL,
+    amount FLOAT NOT NULL,
+    status VARCHAR NOT NULL,
+    description VARCHAR,
+    store VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (credit_card_id) REFERENCES credit_cards(id)
+);
--- codebank/usecase/process_transaction.go
+++ codebank/usecase/process_transaction.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"time"
+
 	"github.com/codeedu/codebank/domain"
 	"github.com/codeedu/codebank/dto"
 )
@@ -14,5 +16,47 @@ func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) U
 }
 
 func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
+	creditCard := u.hydrateCreditCard(transactionDto)
+	ccBalanceAndLimit, err := u.TransactionRepository.GetCreditCard(*creditCard)
 
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	creditCard.ID = ccBalanceAndLimit.ID
+	creditCard.Limit = ccBalanceAndLimit.Limit
+	creditCard.Balance = ccBalanceAndLimit.Balance
+
+	t := u.newTransaction(transactionDto, ccBalanceAndLimit)
+	t.ProcessAndValidate(creditCard)
+
+	err = u.TransactionRepository.SaveTransaction(*t, *creditCard)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	return *t, nil
+
+}
+
+func (u UseCaseTransaction) hydrateCreditCard(transactionDto dto.Transaction) *domain.CreditCard {
+	creditCard := domain.NewCreditCard()
+	creditCard.Name = transactionDto.Name
+	creditCard.Number = transactionDto.Number
+	creditCard.ExpirationMonth = transactionDto.ExpirationMonth
+	creditCard.ExpirationYear = transactionDto.ExpirationYear
+	creditCard.CVV = transactionDto.CVV
+
+	return creditCard
+}
+
+func (u UseCaseTransaction) newTransaction(transactionDto dto.Transaction, cc domain.CreditCard) *domain.Transaction {
+	t := domain.NewTransaction()
+	t.CreditCardId = cc.ID
+	t.Amount = transactionDto.Amount
+	t.Store = transactionDto.Store
+	t.Description = transactionDto.Description
+	t.CreatedAt = time.Now()
+
+	return t
 }
