fed300ac0c2fff75fb967d342b6ed61071f710c6
unknown <flaviosenne123@gmail.com>
2022-08-18 11:43:20 -0300

    adicionar gRPC aula dois

---
+++ codebank/infra/grpc/service/transaction.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+
+	"github.com/codeedu/codebank/dto"
+	"github.com/codeedu/codebank/infra/grpc/pb"
+	"github.com/codeedu/codebank/usecase"
+)
+
+type TransactionService struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewTransactionService() *TransactionService {
+	return &TransactionService{}
+}
+
+func (t *TransactionService) Payment(ctx context.Context, in *pb.PaymentRequest) (*empty.Empty, error) {
+	transactionDto := dto.Transaction{
+		Name:            in.GetCreditCard().GetName(),
+		Number:          in.GetCreditCard().GetNumber(),
+		ExpirationMonth: in.GetCreditCard().GetExpirationMonth(),
+		ExpirationYear:  in.GetCreditCard().GetExpirationYear(),
+		CVV:             in.GetCreditCard().GetCvv(),
+		Amount:          in.GetAmount(),
+		Store:           in.GetStore(),
+		Description:     in.GetDescription(),
+	}
+
+	transaction, err := t.ProcessTransactionUseCase.ProcessTransaction(transactionDto)
+
+	if err != nil {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, err.Error())
+	}
+	if transaction.Status != "approved" {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, "transaction rejected by the bank")
+	}
+
+	return &empty.Empty{}, nil
+}
